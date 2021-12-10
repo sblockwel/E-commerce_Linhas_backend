a69894cb627bb0f859be6b778799db157efcc4d3
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-09 23:06:05 -0300

    Adicionado suporte ao swagger

--- ecommerce/src/main/java/com/lb/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/lb/ecommerce/EcommerceApplication.java
@@ -1,8 +1,11 @@
 package com.lb.ecommerce;
 
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Linhas Blumenau API", version = "1.0", description = "E-commerce de linhas"))
 @SpringBootApplication
 public class EcommerceApplication {
 
