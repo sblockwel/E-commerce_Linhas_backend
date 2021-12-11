eaca29c99ca51b1532f77b15ba1694ca83f65f9e
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-11 17:15:32 -0300

    Revert "Adicionado suporte ao swagger"
    This reverts commit a69894cb627bb0f859be6b778799db157efcc4d3.

--- ecommerce/src/main/java/com/lb/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/lb/ecommerce/EcommerceApplication.java
@@ -1,14 +1,11 @@
 package com.lb.ecommerce;
 
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@OpenAPIDefinition(info = @Info(title = "Linhas Blumenau API", version = "1.0", description = "E-commerce de linhas"))
 @SpringBootApplication
 public class EcommerceApplication {
 
